9e0538ee55f55f863789b9ff167577575fdecfcb
zhyjor <zhyjor@163.com>
2018-11-09 15:03:06 +0800

    add

---
+++ app/public/build/precache-manifest.5d106f05829bc0d3751f674fc9b5876c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ef7d4d77e9ecd2074478",
+    "url": "./static/js/main.ef7d4d77.chunk.js"
+  },
+  {
+    "revision": "5e0368daec98ab2db7a3",
+    "url": "./static/js/1.5e0368da.chunk.js"
+  },
+  {
+    "revision": "ef7d4d77e9ecd2074478",
+    "url": "./static/css/main.67d01d73.chunk.css"
+  },
+  {
+    "revision": "5e0368daec98ab2db7a3",
+    "url": "./static/css/1.c9aff85e.chunk.css"
+  },
+  {
+    "revision": "2d1a8217b1137adfb9c1bfec5976a528",
+    "url": "./index.html"
+  }
+];
